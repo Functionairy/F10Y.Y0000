5fb9d5aa6a3526faa297b539635220ba0972ffb7
SURFEIT\David <david.coats@gmail.com>
2025-03-23 11:50:16 -0700

    Add null-check related documentation.

--- source/F10Y.Y0000/Code/Documentation.cs
+++ source/F10Y.Y0000/Code/Documentation.cs
@@ -1,12 +1,22 @@
 using System;
 
+using F10Y.T0001;
+
 
 namespace F10Y.Y0000
 {
 	/// <summary>
 	/// C#/.NET Standard 2.1 documentation library.
 	/// </summary>
+	[DocumentationsMarker]
 	public static partial class Documentation
 	{
-	}
+        /// <summary>
+        /// When equating two reference type instances, if one or both of the instances are null then a simple null check has determined whether the instances are equal.
+        /// In the case where one instance is null, but the other isn't, a null check has determined equality and the instances are not equal.
+        /// If both are null, then the null check has determined equality and the instances are equal.
+        /// Only in the case where both are not null does a null check not determine equality.
+        /// </summary>
+        public static readonly object NullCheckDeterminesEquality;
+    }
 }
