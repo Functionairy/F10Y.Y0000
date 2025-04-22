dc9f2a25864860f56b6015782cea3f22b3203062
SURFEIT\David <david.coats@gmail.com>
2025-04-22 11:37:12 -0700

    Add documentations for some characters and strings.

--- source/F10Y.Y0000/Code/Documentation/For_Characters.cs
+++ source/F10Y.Y0000/Code/Documentation/For_Characters.cs
@@ -14,6 +14,12 @@ namespace F10Y.Y0000
         public static partial class For_Characters
         {
             /// <summary>
+            /// <description><value>'\n'</value>, <name>new line</name></description>
+            /// <para><description>The '"' quotation mark (or quote or double-quotation mark) character.</description></para>
+            /// </summary>
+            public static readonly object For_NewLine;
+
+            /// <summary>
             /// <para><value>'"'</value>, <name>quotation mark</name></para>
             /// <para><description>The '"' quotation mark (or quote or double-quotation mark) character.</description></para>
             /// </summary>
--- source/F10Y.Y0000/Code/Documentation/For_Strings.cs
+++ source/F10Y.Y0000/Code/Documentation/For_Strings.cs
@@ -14,16 +14,59 @@ namespace F10Y.Y0000
         public static partial class For_Strings
         {
             /// <summary>
+            /// <para><value>"©"</value>, <name>copyright string</name></para>
+            /// <para><description>The "©" copyright string.</description></para>
+            /// </summary>
+            public static readonly object For_Copyright;
+
+            /// <summary>
             /// <para><value>""</value>, <name>empty string</name></para>
             /// <para><description>The "" empty string.</description></para>
             /// </summary>
             public static readonly object For_Empty;
 
             /// <summary>
+            /// <para><value>"\n"</value>, <name>new line</name></para>
+            /// <para><description>The "\n" new line string (or line feed LF).</description></para>
+            /// </summary>
+            public static readonly object For_NewLine;
+
+            /// <summary>
+            /// <para><value>"\r\n"</value>, <name>carriage return and new line</name></para>
+            /// <para><description>The "\r\n" carriage return and new line string (or carriage return and line feed, CRLF).</description></para>
+            /// </summary>
+            public static readonly object For_NewLine_Windows;
+
+            /// <summary>
             /// <para><value>"""</value>, <name>quotation mark</name></para>
             /// <para><description>The """ quotation mark (or quote-mark or double-quotation mark) string.</description></para>
             /// </summary>
             public static readonly object For_QuotationMark;
+
+            /// <summary>
+            /// <para><value>";"</value>, <name>semicolon</name></para>
+            /// <para><description>The ";" semicolon string.</description></para>
+            /// </summary>
+            public static readonly object For_Semicolon;
+
+            /// <summary>
+            /// <para><value>" "</value>, <name>space</name></para>
+            /// <para><description>The " " space string.</description></para>
+            /// </summary>
+            public static readonly object For_Space;
+
+
+            /// <summary>
+            /// <para><value>"false"</value>, <name>false-lowercase</name></para>
+            /// <para><description>The "false" string.</description></para>
+            /// </summary>
+            public static readonly object For_False_Lowercase;
+
+            /// <summary>
+            /// <para><value>"true"</value>, <name>true-lowercase</name></para>
+            /// <para><description>The "true" string.</description></para>
+            /// </summary>
+            public static readonly object For_True_Lowercase;
         }
     }
 }
