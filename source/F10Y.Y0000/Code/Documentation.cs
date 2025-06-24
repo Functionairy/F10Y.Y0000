a496be8a8d3269135968e9154fc6cb6b9319c36f
SURFEIT\David <david.coats@gmail.com>
2025-06-24 12:25:41 -0700

    Add empty interface-as-placeholder and .NET target frameworks link.

--- source/F10Y.Y0000/Code/Documentation.cs
+++ source/F10Y.Y0000/Code/Documentation.cs
@@ -11,6 +11,12 @@ namespace F10Y.Y0000
 	[DocumentationsMarker]
 	public static partial class Documentation
 	{
+        /// <remarks>
+        /// Empty interface to serve as a placeholder.
+        /// All functionality is delivered by implementations.
+        /// </remarks>
+        public static readonly object EmptyInterfaceAsPlaceholder;
+
         /// <summary>
         /// When equating two reference type instances, if one or both of the instances are null then a simple null check has determined whether the instances are equal.
         /// In the case where one instance is null, but the other isn't, a null check has determined equality and the instances are not equal.
@@ -18,5 +24,10 @@ namespace F10Y.Y0000
         /// Only in the case where both are not null does a null check not determine equality.
         /// </summary>
         public static readonly object NullCheckDeterminesEquality;
+
+        /// <summary>
+        /// <link><see href="https://learn.microsoft.com/en-us/dotnet/standard/frameworks#supported-target-frameworks"/></link>
+        /// </summary>
+        public static readonly object TargetFrameworks_Link;
     }
 }
